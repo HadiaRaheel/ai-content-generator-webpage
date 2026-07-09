8d4dab08a9a091da2f84f6f08bc40927aeb2994f
HadiaRaheel <i222700@nu.edu.pk>
2026-07-09 16:09:33 +0500

    Changes images due to jfif extension issue

--- data/images.js
+++ data/images.js
@@ -1,16 +1,16 @@
 const images = [
   '/images/model16.webp',
-  '/images/model18.jfif',
-  '/images/model15.jfif',
-  '/images/model17.jfif',
+  '/images/model1.jpg',
+  '/images/model3.jpg',
+  '/images/model4.jpg',
+  '/images/model6.jpg',
   '/images/model16.webp',
-  '/images/model18.jfif',
-  '/images/model15.jfif',
-  '/images/model17.jfif',
+  '/images/model1.jpg',
+  '/images/model3.jpg',
+  '/images/model4.jpg',
+  '/images/model6.jpg',
   '/images/model16.webp',
-  '/images/model18.jfif',
-  '/images/model15.jfif',
-  '/images/model17.jfif',
+  '/images/model1.jpg',
 ];
 
 export default images;
